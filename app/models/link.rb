0a741704f350d9457cb3cf4ae0527ac996a60c77
Tijmen de Graaff <tijmen76@gmail.com>
2017-03-07 18:20:29 +0100

    added url validation in link-model

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,17 @@ class Link < ApplicationRecord
   acts_as_votable
   belongs_to :user
   has_many :comments
+  before_validation :review_url
 end
+
+private
+
+def review_url
+    if url =~ /^(http:\/\/)/
+      self.url = url
+    elsif url =~ /^(https:\/\/)/
+      self.url = url
+    else
+      self.url = "http://#{url}"
+    end
+  end
